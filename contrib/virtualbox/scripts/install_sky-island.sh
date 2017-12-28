03d34d9708f3c3d5a1b28a32a9f1861a19c3ec72
Brian Downs <brian.downs@gmail.com>
2017-12-28 14:12:25 -0700

    sky island virtualbox install script

--- contrib/virtualbox/scripts/install_sky-island.sh
+++ contrib/virtualbox/scripts/install_sky-island.sh
@@ -5,11 +5,17 @@ set -ex
 echo "Installing monitoring software"
 pkg install -y influxdb telegraf grafana4
 
-echo "Installing Sky Island"
-# git clone
-# make install
-# /usr/local/etc/rc.d/influxd start
-# /usr/local/etc/rc.d/telegraf start
-# /usr/local/etc/rc.d/grafana start
+echo "Installing Sky Island..."
+git clone https://github.com/briandowns/sky-island.git
+cd sky-island
+make install
+
+echo "Starting supplimentary services..."
+/usr/local/etc/rc.d/influxd start
+/usr/local/etc/rc.d/telegraf start
+/usr/local/etc/rc.d/grafana start
+
+echo "Starting Sky Island..."
+/usr/local/etc/rc.d/sky-island start
 
 exit 0
